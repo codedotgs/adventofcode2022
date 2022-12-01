5a2dd8a42360058f81b992b6142b27eeb6316caa
CodeDotGS <codedotgs@gmail.com>
2022-12-02 00:05:42 +0100

    day 1 done

---
+++ day1.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+
+const content = fs.readFileSync("./day1-input.txt", "utf8");
+
+const inventory = content.split("\n");
+
+const totalCaloriesByElf = [];
+let calories = 0;
+
+for (let i = 0; i < inventory.length; i++) {
+  if (!inventory[i].length) {
+    totalCaloriesByElf.push(calories);
+    calories = 0;
+  } else {
+    calories += parseInt(inventory[i], 10);
+  }
+}
+
+const highest = Math.max(...totalCaloriesByElf);
+
+console.log({ highest });
+
+const top3 = [0, 0, 0];
+for (let i = 0; i < totalCaloriesByElf.length; i++) {
+  let minimal = Math.min(...top3);
+  if (minimal < totalCaloriesByElf[i]) {
+    minimalIndex = top3.indexOf(minimal);
+    top3[minimalIndex] = totalCaloriesByElf[i];
+  }
+}
+
+console.log({ top3 });
+console.log(top3[0] + top3[1] + top3[2]);
